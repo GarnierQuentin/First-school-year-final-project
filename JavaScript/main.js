086a4c682a6add1820d69b4561203ccf7e9f60fd
F1N3X <quartzman92@gmail.com>
2023-02-02 23:38:58 +0100

    fonction de création de posts

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -26,9 +26,6 @@ const removePostByTrash = (trash) => {
 
     const post = profile.parentNode
     console.log(post)
-
-    const childNodes = post.childNodes
-    console.log(childNodes)
     
     const nameOfTag = post.querySelector(".nameOfTag")
 
@@ -41,6 +38,92 @@ const removePostByTrash = (trash) => {
     post.removeChild(text)
     post.removeChild(image)
     post.remove()
-    console.log(`child Nodes : ${childNodes}`)
 
 }
+
+
+const do_a_post = document.getElementById("do_a_post")
+const publish = document.getElementById("publish")
+
+do_a_post.addEventListener("click", () => {
+    publish.style.display = "block"
+})
+
+
+
+
+let textarea = document.getElementById("content_text_input")
+let text_post = textarea.value
+console.log(text_post)
+
+textarea.addEventListener("input", () => {
+    text_post = textarea.value
+    console.log(text_post)
+})
+
+
+
+
+let tag = document.getElementById("liste_tags")
+let value_tag = tag.value
+let text_tag = tag.options[tag.selectedIndex].text
+console.log(value_tag)
+
+tag.addEventListener("input", () => {
+    value_tag = tag.value
+    text_tag = tag.options[tag.selectedIndex].text
+    console.log(`Le tag actuel ${value_tag}`)
+    console.log(`Son type : ${typeof text_tag}`)
+})
+
+
+
+const publish_button = document.getElementById("submit_button")
+const cancel_button = document.getElementById("cancel_button")
+const feed = document.getElementById("post_content")
+
+cancel_button.addEventListener("click", () => {
+    publish.style.display = "none"
+})
+
+publish_button.addEventListener("click", () => {
+    console.log("pas encore dans la condition")
+        console.log("la fenêtre doit se fermer")
+        feed.insertAdjacentHTML("afterbegin", `<div class="post">
+
+        <div class="profile">
+
+            <div class="identity">
+
+                <div class="mini_profile_picture">
+                    <img class="photo" src="assets/photos de profile/yugo_TP.jpg" alt="mini photo de profile">
+                </div>
+
+                <div class="username">
+                    Quentin
+                </div>
+
+            </div>
+
+            <div class="trash">
+                <img src="assets/icones/poubelle.png" alt="">
+            </div>
+
+        </div>
+
+        <div class="nameOfTag">
+            #${text_tag}
+        </div>
+
+        <div class="text">
+            ${text_post}
+        </div>
+
+        <div class="image">
+            
+        </div>
+
+    </div>`)
+    publish.style.display = "none"
+    
+})
